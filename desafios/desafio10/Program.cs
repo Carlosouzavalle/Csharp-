30ee0857867ca020ab60b1e0561c700d602d5b92
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-19 01:14:36 -0300

    ✨ feat: desafios 9 e 10 concluidos

---
+++ desafios/desafio10/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Quantos dolares você quer comprar? ");
+string entrada = Console.ReadLine();
+
+if(decimal.TryParse(entrada, out decimal resuult))
+{
+    decimal cotacao = 5.25m;
+    decimal valorEmDolares = resuult / cotacao;
+    //Console.WriteLine(resuult);
+    Console.WriteLine($"Com tantos {entrada}R$ você pode comprar {valorEmDolares:F2} Dolares");
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+
+// {valorEmDolares.ToString("C", new System.Globalization.CultureInfo("pt-BR"))} Dolares")
+// essa linha vai formatar o valor em dolares para o formato brasileiro
+
+// Segunda Opção 
+// {valorEmDolares:F2}
