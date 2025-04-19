a3b43832d47bfdac6cb527432d0d76147d3885b1
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-19 00:43:37 -0300

    Alguns desafios Resolvidos

---
+++ desafios/desafio9/Program.cs
@@ -0,0 +1,11 @@
+﻿string valor = Console.ReadLine();
+
+if(int.TryParse(valor, out int numero))
+{
+    for(int i = 1; i <= 10; i++)
+    {
+       Console.WriteLine($"{valor} x {i} - {i * numero}");
+    }
+}
+
+Console.ReadKey();
