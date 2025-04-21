3505149f4d45c8bde3eadff7020afbf7386e1ef3
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-20 21:19:17 -0300

    ✨ feat: mais alguns exercicios resolvidos

---
+++ desafios/desafio19/Program.cs
@@ -0,0 +1,4 @@
+﻿Random random = new Random();
+
+int numeroSorteado = random.Next(1, 10);
+Console.WriteLine($"O número sorteado foi: {numeroSorteado}");
---
+++ desafios/desafio20/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> alunos = new List<string> { "Carlos", "Bia", "Camila" };
+Random random = new Random();
+
+for(int i = 0; i < alunos.Count; i++)
+{
+    int j = random.Next(i, alunos.Count);
+    string temp = alunos[i];
+    alunos[i] = alunos[j];
+    alunos[j] = temp;
+}
+
+
+Console.WriteLine("A ordem de apresentação dos alunos é:");
+foreach (string aluno in alunos)
+{
+    Console.WriteLine(aluno);
+}
+
+
+Console.ReadKey();
