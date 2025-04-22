a9bafff8da64f299e952fe201e5b195593916b6d
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-22 20:22:15 -0300

    ✨ feat: desafio 22 resolvido

---
+++ desafios/desafio22/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Analizando seu texto...");
+Console.WriteLine($"Seu nome tem {nome.Length} letras.");
+string primeiroNome = nome.Split(' ')[0]; // pega o primeiro nome antes do espaço
+Console.WriteLine($"Seu primeiro nome é {primeiroNome} e tem {primeiroNome.Length} letras.");
+Console.WriteLine($" Seu nome em maisusculo è {nome.ToUpper()}");
+Console.WriteLine($"Seu nome em minusculo é {nome.ToLower()}");
