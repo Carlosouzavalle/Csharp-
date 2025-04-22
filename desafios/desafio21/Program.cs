70109f973828419192aaea30d045b9f529f25225
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-21 21:02:00 -0300

    ✨ feat: mais um desafio resolvido

---
+++ desafios/desafio21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NAudio.Wave;
+
+class Program
+{
+    static void Main()
+    {
+        using (var audioFile = new AudioFileReader("AQUELAS COISAS - João Gomes e Tarcísio do Acordeon.mp3"))
+        using (var outputDevice = new WaveOutEvent())
+        {
+            outputDevice.Init(audioFile);
+            outputDevice.Play();
+
+            Console.WriteLine("Tocando música...");
+            Console.ReadKey(); // Pausa até você apertar uma tecla
+        }
+    }
+}
