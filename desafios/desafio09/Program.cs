65fb5bc9f4d6b5dbd2ba2eeff68ad4d96a38275d
carlosouzavalle <carlosouzavalle@gmail.com>
2026-07-09 21:07:20 -0300

    desafios

---
+++ desafios/desafio09/Program.cs
@@ -0,0 +1,29 @@
+﻿// double valor = 2;
+
+
+// for(int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine($"{valor} x {i} = {valor * i}");
+// }
+
+
+// Em C# quando queremos usar template string utilizamos o simbolo $, porem diferente do JavaScript que usa ` essa forma ` em C# usamos aspas duplas " " para definir a string.
+
+Console.WriteLine("Digite um valor para ver a tabuada: ");
+double valor = double.Parse(Console.ReadLine());
+
+
+while (valor <= 0 || valor >= 1)
+{
+    double tabuada = 2;
+    while(tabuada <= 10)
+    {
+        Console.WriteLine($"{valor} x {tabuada} = {valor * tabuada}");
+        tabuada++;
+    }
+
+    
+    Console.WriteLine("Digite um valor: ");
+    valor = double.Parse(Console.ReadLine());
+    
+}
---
+++ desafios/desafio10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite um valor: ");
+double valor = double.Parse(Console.ReadLine());
+
+double dolar = 5.25;
+Console.WriteLine($"O valor de {valor} você pode comprar {valor / dolar:F2} dólares");
