179487296407766138c5e0d720754b7c3b7b1b84
carlosouzavalle <carlosouzavalle@gmail.com>
2026-08-21 23:30:02 -0300

    .

---
+++ ConceitosBasicos/ARRAYS/Arrays.cs
@@ -0,0 +1,33 @@
+namespace ConceitosBasicos
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = [1,2,3,4,5];
+            string[] frutas = ["maça", "banana", "uva", "abacaxi" ];
+
+//            Console.WriteLine(valores[2]);
+
+            // foreach(int mostrarValores in valores)
+            // {
+            //     Console.WriteLine(mostrarValores);
+            // }
+
+
+            // for(int i = 0; i < frutas.Length; i++)
+            // {
+            //     Console.WriteLine(frutas[i]);
+            // }
+
+
+            frutas[2] = "laranja"; 
+            // Console.WriteLine(frutas[2]);
+
+            for(int i = 0; i < frutas.Length; i++)
+            {
+                Console.WriteLine($"O Indice é {i}: " + frutas[i]);
+            }
+        }
+    }
+}
